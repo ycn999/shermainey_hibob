a38782b2f14761655e6f7b7e2fce3bc6c45200e9
Shermaine Yung <136255659+ycn999@users.noreply.github.com>
2024-02-15 07:32:14 +0800

    Update q1.sql

--- q1.sql
+++ q1.sql
@@ -1,51 +1,51 @@
-WITH account_latest AS (
+WITH accounts AS (
 SELECT 
-	arr.account_name,
-	arr.size,
-	pss.status AS ps_status,
+  acc.account_name,
+  acc.size,
+  pss.status AS ps_status,
   status_date AS ps_status_date,
-  arr.arr,
+  acc.arr,
   cycle_status,
   cycle_date,
-  ROW_NUMBER() OVER (PARTITION BY arr.account_name ORDER BY cycle_date DESC) AS latest_cycle_date_check
-FROM `studied-zephyr-393513.hibob_trial.account_arr` arr
-LEFT JOIN `studied-zephyr-393513.hibob_trial.account_cycle` cycle ON arr.account_name = cycle.account_name
-LEFT JOIN `studied-zephyr-393513.hibob_trial.account_ps` ps ON arr.account_name = ps.account_name
-LEFT JOIN `studied-zephyr-393513.hibob_trial.ps_status` pss ON ps.ps_id = pss.ps_id
+  ROW_NUMBER() OVER (PARTITION BY acc.account_name ORDER BY cycle_date DESC) AS latest_cycle_date_check
+FROM `account_arr` acc
+LEFT JOIN `account_cycle` cycle ON acc.account_name = cycle.account_name
+LEFT JOIN `account_ps` ps ON acc.account_name = ps.account_name
+LEFT JOIN `ps_status` pss ON ps.ps_id = pss.ps_id
 ),
 
 arr_dec_2023 AS (
 SELECT
  account_name,
- arr.arr
-FROM `studied-zephyr-393513.hibob_trial.account_arr` arr
+ acc.arr
+FROM `account_arr` acc
 WHERE month = '2023-12-01'
 ),
 
-account_last_approved AS (
+last_approved AS (
 SELECT
   account_name,
   MAX(cycle_date) AS last_approved_comp_cycle
-FROM `studied-zephyr-393513.hibob_trial.account_cycle` cycle
+FROM `account_cycle` cycle
 WHERE cycle_status = 'Approved'
 GROUP BY account_name
 )
 
 SELECT 
   size,
-  account_latest.account_name,
+  accounts.account_name,
   CASE 
     WHEN ps_status IS NULL THEN 'No Implementation'
     WHEN ps_status = 'In Progress' THEN 'In Implementation'
-    WHEN ps_status = 'Completed' AND DATE_DIFF(CURRENT_DATE(), ps_status_date, day) > 365 THEN 'Over 1 year since Implementation'
-    WHEN ps_status = 'Completed' AND DATE_DIFF(CURRENT_DATE(), ps_status_date, day) <= 365 THEN 'Under 1 year since Implementation'
+    WHEN ps_status = 'Completed' AND DATE_DIFF(CURRENT_DATE(), ps_status_date, day) >= 365 THEN 'Over 1 year since Implementation'
+    WHEN ps_status = 'Completed' AND DATE_DIFF(CURRENT_DATE(), ps_status_date, day) < 365 THEN 'Under 1 year since Implementation'
   END AS implementation_indication,
   last_approved_comp_cycle,
   cycle_status AS last_comp_cycle_status,
   cycle_date AS last_comp_cycle_date,
   arr_dec_2023.arr
-FROM account_latest
-JOIN arr_dec_2023 ON account_latest.account_name = arr_dec_2023.account_name
-FULL OUTER JOIN account_last_approved ON account_latest.account_name = account_last_approved.account_name
+FROM accounts
+JOIN arr_dec_2023 ON accounts.account_name = arr_dec_2023.account_name
+FULL OUTER JOIN last_approved ON accounts.account_name = last_approved.account_name
 WHERE latest_cycle_date_check = 1 
 ORDER BY account_name
